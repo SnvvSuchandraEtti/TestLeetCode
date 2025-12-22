5c599ffe174d4409a08f26aaceb30dc6a578e5dd
ETTI S N V V SUCHANDRA <22A91A0570@aec.edu.in>
2025-12-22 12:48:01 +0530

    Time: 2 ms (56.35%), Space: 43.8 MB (16.47%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,22 +1,31 @@
 class Solution {
-        public boolean isMatch(String s, String p) {
-                int m = s.length(), n = p.length();
-                        boolean[][] f = new boolean[m + 1][n + 1];
-                                f[0][0] = true;
-                                        for (int i = 0; i <= m; ++i) {
-                                                    for (int j = 1; j <= n; ++j) {
-                                                                    if (p.charAt(j - 1) == '*') {
-                                                                                        f[i][j] = f[i][j - 2];
-                                                                                                            if (i > 0 && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))) {
-                                                                                                                                    f[i][j] |= f[i - 1][j];
-                                                                                                                                                        }
-                                                                                                                                                                        } else if (i > 0
-                                                                                                                                                                                            && (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1))) {
-                                                                                                                                                                                                                f[i][j] = f[i - 1][j - 1];
-                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                            return f[m][n];
-                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        
+        // Handle patterns like a*, a*b*, a*b*c* etc
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    // Two cases: skip the element or match current
+                    dp[i][j] = dp[i][j - 2]; // skip x*
+                    if (p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j]; // match one or more
+                    }
+                } else if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
 }
