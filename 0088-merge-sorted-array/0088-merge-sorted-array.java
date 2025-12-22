0ae5e7f876610d40ac035e9575f5c5ad5dfc16e7
ETTI S N V V SUCHANDRA <22A91A0570@aec.edu.in>
2025-12-22 13:05:58 +0530

    Time: 0 ms (100%), Space: 43.6 MB (73.2%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -0,0 +1,1 @@
+class Solution{public void merge(int[]n1,int m,int[]n2,int n){int p1=m-1,p2=n-1,p=m+n-1;while(p1>=0&&p2>=0){if(n1[p1]>n2[p2]){n1[p]=n1[p1];p1--;}else{n1[p]=n2[p2];p2--;}p--;}while(p2>=0){n1[p]=n2[p2];p2--;p--;}}}
