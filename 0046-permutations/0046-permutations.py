4de2d693ad6adfc971575dd72a7a103e1bcb73dd
ETTI S N V V SUCHANDRA <22A91A0570@aec.edu.in>
2025-12-22 14:20:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,20 @@
+def permute(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: List[List[int]]
+    """
+    result = []
+    
+    def backtrack(current):
+        if len(current) == len(nums):
+            result.append(current[:])
+            return
+        
+        for num in nums:
+            if num not in current:
+                current.append(num)
+                backtrack(current)
+                current.pop()
+    
+    backtrack([])
+    return result
