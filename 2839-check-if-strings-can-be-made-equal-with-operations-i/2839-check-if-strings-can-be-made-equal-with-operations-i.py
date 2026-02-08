9c92625e0b4d898a0961348b3a4df237146f4735
ETTI S N V V SUCHANDRA <22A91A0570@aec.edu.in>
2026-02-08 15:18:23 +0530

    Time: 1 ms (54.29%), Space: 12.5 MB (48.57%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canBeEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        # Characters at even indices can be swapped with each other
+        # Characters at odd indices can be swapped with each other
+        even_s1 = sorted([s1[0], s1[2]])
+        even_s2 = sorted([s2[0], s2[2]])
+        odd_s1 = sorted([s1[1], s1[3]])
+        odd_s2 = sorted([s2[1], s2[3]])
+        
+        return even_s1 == even_s2 and odd_s1 == odd_s2
